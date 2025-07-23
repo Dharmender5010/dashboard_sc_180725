aa396d034fbeb01b240ca9841ac95bb1e3938ec5
Dharmender5010 <sonumehra5010@gmail.com>
2025-07-23 14:13:50 +0530

    Create config.ts

---
+++ services/config.ts
@@ -0,0 +1,4 @@
+// --- CONFIGURATION ---
+// PASTE YOUR SINGLE, CONSOLIDATED, DEPLOYED GOOGLE APPS SCRIPT WEB APP URL HERE.
+// This is the only place you need to update the URL for all services.
+export const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxcx5ZxOFsKWoo1zndFByjGUU2fcacr4HabltYalAofYYGckJBYxygPJYEl5R7wz-p4/exec';
